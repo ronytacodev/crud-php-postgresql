cab5253bb58728563cf111e31935cb96b0e84211
RonyTCdev <rtcardenas26@gmail.com>
2023-05-18 14:27:57 -0500

    upload project

---
+++ README.md
@@ -0,0 +1,19 @@
+# CRUD PostgreSQL WITH PHP
+
+How to create a CRUD with PHP and POSTGRESQL.
+
+## TECH & TOOLS :wrench:
+
+* PHP
+* POSTGRESQL
+* Bootstrap
+
+## SCREENSHOT
+
+![](assets/screenshot1.jpg)
+
+![](assets/screenshot2.jpg)
+
+![](assets/screenshot3.jpg)
+
+![](assets/screenshot4.jpg)
---
+++ base_de_datos.php
@@ -0,0 +1,19 @@
+<?php
+/*
+================================
+Este archivo se encarga de conectar a la base de datos
+y traer un objeto PDO
+================================
+ */
+$contraseña = "Vonex$2023";
+$usuario = "postgres";
+$nombreBaseDeDatos = "crud_mascotas";
+# Puede ser 127.0.0.1 o el nombre de tu equipo; o la IP de un servidor remoto
+$rutaServidor = "127.0.0.1";
+$puerto = "5432";
+try {
+    $base_de_datos = new PDO("pgsql:host=$rutaServidor;port=$puerto;dbname=$nombreBaseDeDatos", $usuario, $contraseña);
+    $base_de_datos->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    echo "Ocurrió un error con la base de datos: " . $e->getMessage();
+}
